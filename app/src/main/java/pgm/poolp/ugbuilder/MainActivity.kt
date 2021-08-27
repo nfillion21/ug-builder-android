e358e643eb4b7ff2cb0c58e44725a407b9dac3b0
Nicolas Fillion <nfillion21@gmail.com>
2021-08-27 15:23:16 +0200

    dependencies added

--- app/src/main/java/pgm/poolp/ugbuilder/MainActivity.kt
+++ app/src/main/java/pgm/poolp/ugbuilder/MainActivity.kt
@@ -14,7 +14,6 @@ import pgm.poolp.ugbuilder.ui.theme.UGBuilderTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*
         setContent {
             UGBuilderTheme {
                 // A surface container using the 'background' color from the theme
@@ -23,14 +22,15 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        */
 
         // This app draws behind the system bars, so we want to handle fitting system windows
+        /*
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
             OwlApp { finish() }
         }
+         */
     }
 }
 
