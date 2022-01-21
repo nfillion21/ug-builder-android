35e3d7e79531acb17bb92fe517872ca12f6f7a93
Nicolas Fillion <nfillion21@gmail.com>
2022-01-21 18:23:26 +0100

    display filter buttons

--- app/src/main/java/pgm/poolp/ugbuilder/ui/players/SearchPlayers.kt
+++ app/src/main/java/pgm/poolp/ugbuilder/ui/players/SearchPlayers.kt
@@ -1,20 +1,27 @@
 package pgm.poolp.ugbuilder.ui.players
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Reorder
+import androidx.compose.material.icons.filled.Sort
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.insets.statusBarsPadding
+import pgm.poolp.ugbuilder.preferences.SortOrder
 import pgm.poolp.ugbuilder.viewmodels.HeroViewModel
 
 @Composable
@@ -22,28 +29,142 @@ fun SearchPlayers(
     heroViewModel: HeroViewModel,
     modifier: Modifier = Modifier
 ) {
-    //val newHeroViewModel:HeroViewModel = viewModel()
-    //val heroes = newHeroViewModel.allHeroes.observeAsState()
-    val heroes by heroViewModel.allPlayers.observeAsState(listOf())
-    LazyColumn(modifier = modifier.statusBarsPadding()) {
-        item {
-            CoursesAppBar()
-        }
-        items(heroes) { hero ->
-            Text(
-                text = hero.name,
-                style = MaterialTheme.typography.h5,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable(onClick = { /* todo */ })
-                    .padding(
-                        start = 16.dp,
-                        top = 8.dp,
-                        end = 16.dp,
-                        bottom = 8.dp
+    val heroes by heroViewModel.filteredPlayers(showVillains = true, sortOrder = SortOrder.BY_NAME)
+        .observeAsState(listOf())
+
+    Column(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth()
+        .statusBarsPadding())
+    {
+
+
+        LazyColumn() {
+            item {
+                CoursesAppBar()
+            }
+
+            item {
+
+                Row(modifier = Modifier.padding(all = 16.dp)) {
+                    Icon(
+                        imageVector = Icons.Default.Reorder,
+                        contentDescription = null,
+                        tint = LocalContentColor.current.copy(alpha = 0.5f),
                     )
-                    .wrapContentWidth(Alignment.Start)
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    SortChip(
+                        text = "Priority",
+                        selected = true,
+                        //setSelected = ,
+                        shape = RoundedCornerShape(14.dp)
+                    )
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    SortChip(
+                        text = "Deadline",
+                        selected = true,
+                        //setSelected = ,
+                        shape = RoundedCornerShape(14.dp)
+                    )
+                }
+            }
+
+            item {
+
+                Row(modifier = Modifier.padding(all = 16.dp)) {
+                    Icon(
+                        imageVector = Icons.Default.Sort,
+                        contentDescription = null,
+                        tint = LocalContentColor.current.copy(alpha = 0.5f),
+                    )
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    Text(
+                        modifier = Modifier.wrapContentWidth(),
+                        text = "Show completed tasks",
+                        style = MaterialTheme.typography.subtitle1
+                    )
+
+                    Spacer(modifier = Modifier.width(16.dp))
+
+                    Checkbox(
+                        checked = true,
+                        onCheckedChange = null
+                    )
+                }
+            }
+
+            items(heroes) { hero ->
+                Text(
+                    text = hero.name,
+                    style = MaterialTheme.typography.h5,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable(onClick = { /* todo */ })
+                        .padding(
+                            start = 16.dp,
+                            top = 8.dp,
+                            end = 16.dp,
+                            bottom = 8.dp
+                        )
+                        .wrapContentWidth(Alignment.Start)
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun SortChip(
+    text: String,
+    selected: Boolean,
+    //setSelected: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    shape: Shape = MaterialTheme.shapes.small,
+) {
+    Surface(
+        modifier = modifier.height(28.dp),
+        color = MaterialTheme.colors.secondary,
+        shape = shape,
+        elevation = 2.dp
+    ) {
+        Box(
+            modifier = Modifier.toggleable(
+                value = selected,
+                onValueChange = {
+                    println("Toggled!")
+                },
             )
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(
+                    horizontal = 8.dp,
+                    vertical = 6.dp
+                )
+            ) {
+                AnimatedVisibility(visible = selected) {
+                    Icon(
+                        imageVector = Icons.Default.Check,
+                        contentDescription = null,
+                        modifier = Modifier.width(24.dp),
+                    )
+                    Spacer(modifier = Modifier.width(2.dp))
+                }
+
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.caption,
+                    maxLines = 1,
+
+                    )
+            }
         }
     }
 }
